33423a579e85b3a89f06f2413edc7a88324d0857
danilo.souza <danilo.souza@mirante.net.br>
2025-10-16 10:51:50 -0300

    Create spring application

---
+++ StockPrice/src/main/java/djs/inventory/StockPriceApplication.java
@@ -0,0 +1,11 @@
+package djs.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockPriceApplication.class);
+    }
+}
