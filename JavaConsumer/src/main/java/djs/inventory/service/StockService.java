94ea53307afdf9be6534c655865a7a20f77deeb4
danilo.souza <danilo.souza@mirante.net.br>
2025-10-23 11:09:12 -0300

    Removing @Service from interface

--- JavaConsumer/src/main/java/djs/inventory/service/StockService.java
+++ JavaConsumer/src/main/java/djs/inventory/service/StockService.java
@@ -1,9 +1,7 @@
 package djs.inventory.service;
 
 import djs.inventory.dto.StockDTO;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface StockService {
 
     void updateStock(StockDTO stockDTO);
